f22a6f05538f2489d3a27d28688107af0971cb9a
Allassane <dickoallassane1997@gmail.com>
2023-04-25 23:36:38 +0100

    Fix linter errors

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -2,34 +2,35 @@ class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
 
-  validates :quantity, presence: true, numericality: { greater_or_equal_to: 0}
+  validates :quantity, presence: true, numericality: { greater_or_equal_to: 0 }
 
   # recipe_food should include (foods, recipes)
   def value
-    return "no food error" if !self.food
-    self.food.price * self.quantity
+    return 'no food error' unless food
+
+    food.price * quantity
   end
 
   def self.total_price(recipe_foods)
-    total_price = recipe_foods.reduce(0) { |sum, recipe_food| sum += recipe_food.value}
+    recipe_foods.reduce(0) { |sum, recipe_food| sum += recipe_food.value }
   end
 
   def self.shopping_list(user)
     recipe_foods = RecipeFood.joins(:food)
-    .select('recipe_foods.food_id, foods.name, foods.price, SUM(recipe_foods.quantity) as quantity')
-    .group('recipe_foods.food_id, foods.name, foods.price')
-    
-    user_foods = Food.where(user: user)
+      .select('recipe_foods.food_id, foods.name, foods.price, SUM(recipe_foods.quantity) as quantity')
+      .group('recipe_foods.food_id, foods.name, foods.price')
+
+    user_foods = Food.where(user:)
 
     shop_list = []
     recipe_foods.each do |recipe_food|
-      food = user_foods.select {|el| el.id === recipe_food.food_id}
+      food = user_foods.select { |el| el.id === recipe_food.food_id }
       if food.length === 1
         food = food[0]
         recipe_food.quantity -= food.quantity if food
         recipe_food.price *= recipe_food.quantity
 
-        shop_list << recipe_food if recipe_food.quantity > 0
+        shop_list << recipe_food if recipe_food.quantity.positive?
       else
         shop_list << recipe_food
       end

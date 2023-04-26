0d12479f152be4b403fd52b2dcfd65554c92d9ca
Prantosh Biswas <pbiswas1007@gmail.com>
2023-04-26 17:35:47 +0530

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   root "foods#index"
   devise_for :users
-  resources :recipes
+  resources :recipes do
+    resources :recipe_foods, only: [:new, :create, :destroy, :show, :edit, :update]
+  end
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   get "public_recipes", to: "public_recipes#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
 end

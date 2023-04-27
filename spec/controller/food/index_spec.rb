b37fd7d74d424e09a100b5f94f50c5c4a6629ad6
Allassane <dickoallassane1997@gmail.com>
2023-04-27 10:40:20 +0100

    Add integration test for food index

---
+++ spec/controller/food/index_spec.rb
@@ -0,0 +1,31 @@
+require  'rails_helper'
+require_relative '../spec_support_helpers'
+
+
+RSpec.describe FoodsController, type: :system do
+  before(:all) do
+    user = authentificate_test_user
+    @food = Food.first
+    @food ||= Food.create(name: 'Salt', measurement_unit: 'gram', price: 1, quantity: 3, user: user)
+  end
+
+  it 'can see the food index page' do
+    authentificate_test_user
+    expect(page).to have_content("Signed in successfully.")
+    visit '/foods'
+    
+    expect(page).to have_content(@food.name)
+    expect(page).to have_content(@food.measurement_unit)
+    expect(page).to have_content(@food.price)
+    expect(page).to have_content("Remove")
+  end
+
+  it 'Btn add food should navigate to new' do
+    authentificate_test_user
+    expect(page).to have_content("Signed in successfully.")
+    visit '/foods'
+    click_button 'Add Food'
+
+    expect(has_current_path?("/foods/new", wait: 5)).to be_truthy
+  end
+end
---
+++ spec/controller/spec_support_helpers.rb
@@ -0,0 +1,24 @@
+def authentificate_test_user #(current_user)
+  # Your code here
+
+  user = User.find_by(email: 'testemail3@gmail.com')
+
+  if user
+    visit '/users/sign_in'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'testPassword'
+    click_button 'Log in'
+  else
+    # sign up
+    user = User.create(name: 'testName', email: 'testemail3@gmail.com', password: 'testPassword', password_confirmation: 'testPassword')
+    visit '/users/sign_up'
+    fill_in 'Name', with: 'testName'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'testPassword'
+    fill_in 'Password confirmation', with: 'testPassword'
+    click_button 'Sign up'
+  end
+
+  user
+end
+
